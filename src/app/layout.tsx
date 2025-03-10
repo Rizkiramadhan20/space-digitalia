e64965fd3f42d9466b2413aa25b93f7a68e9daf8
Rineta2 <rzkir571@gmail.com>
2025-03-10 16:22:35 +0700

    [update] index

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,46 +1,45 @@
 import { metadata } from "@/base/meta/Metadata";
+import "@/base/style/globals.css";
+import Providers from "@/base/router/Provider";
+import Pathname from "@/base/router/Pathname";
+import { openSans } from "@/base/fonts/Fonts";
+import Script from "next/script";
 
 metadata.manifest = "/manifest.json";
 
 export { metadata };
 
-import "@/base/style/globals.css";
-
-import Providers from "@/base/router/Provider";
-
-import Pathname from "@/base/router/Pathname";
-
-import { openSans } from "@/base/fonts/Fonts";
-
-import Script from "next/script";
-
 export default function RootLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+}: Readonly<{ children: React.ReactNode }>) {
+  const GTM_ID = process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID as string;
+
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        <Script
-          dangerouslySetInnerHTML={{
-            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-M2T56SL8');`,
-          }}
-          id="gtm-script"
-        />
+        {/* Google Tag Manager */}
+        {GTM_ID && (
+          <Script
+            id="google-tag-manager"
+            strategy="afterInteractive"
+            src={`https://www.googletagmanager.com/gtm.js?id=${GTM_ID}`}
+          />
+        )}
       </head>
-      <body
-        className={`${openSans.variable} antialiased`}
-      >
+      <body className={`${openSans.variable} antialiased`}>
         <Providers>
           <Pathname>
             {/* GTM NoScript */}
-            <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-M2T56SL8"
-              height="0" width="0" style={{ display: 'none', visibility: 'hidden' }}></iframe></noscript>
+            {GTM_ID && (
+              <noscript>
+                <iframe
+                  src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+                  height="0"
+                  width="0"
+                  style={{ display: "none", visibility: "hidden" }}
+                ></iframe>
+              </noscript>
+            )}
             {children}
           </Pathname>
         </Providers>
