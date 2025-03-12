ad41649650583cdf45dfa49db86b9a0851c19a08
Rineta2 <rzkir571@gmail.com>
2025-03-12 09:26:43 +0700

    [update] sitemap

--- next.config.ts
+++ next.config.ts
@@ -10,20 +10,6 @@ const nextConfig: NextConfig = {
     skipWaiting: true,
     disable: process.env.NODE_ENV === "development",
   },
-  output: "standalone",
-  async headers() {
-    return [
-      {
-        source: "/sitemap.xml",
-        headers: [
-          {
-            key: "Content-Type",
-            value: "application/xml",
-          },
-        ],
-      },
-    ];
-  },
 };
 
 export default nextConfig;
